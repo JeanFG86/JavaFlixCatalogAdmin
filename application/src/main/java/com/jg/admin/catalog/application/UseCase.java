920f1cabdc92e3294ac784ee1695a65f1d946df1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-12-15 20:34:52 -0300

    create UseCaseTest

---
+++ application/src/main/java/com/jg/admin/catalog/application/UseCase.java
@@ -0,0 +1,12 @@
+package com.jg.admin.catalog.application;
+
+
+import com.jg.admin.catalog.domain.Category;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class UseCase {
+    public Category execute(){
+        return new Category();
+    }
+}
