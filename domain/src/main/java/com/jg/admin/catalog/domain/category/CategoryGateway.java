2b6d7d8343dea4879b89ae49dcb1911ea9495764
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-12-20 18:30:45 -0300

    create CategoryGateway

---
+++ domain/src/main/java/com/jg/admin/catalog/domain/category/CategoryGateway.java
@@ -0,0 +1,16 @@
+package com.jg.admin.catalog.domain.category;
+
+import com.jg.admin.catalog.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+    void deleteById(CategoryID anId);
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
---
+++ domain/src/main/java/com/jg/admin/catalog/domain/pagination/Pagination.java
@@ -0,0 +1,7 @@
+package com.jg.admin.catalog.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(int currentPage, int perPage, long total, List<T> items) {
+
+}
