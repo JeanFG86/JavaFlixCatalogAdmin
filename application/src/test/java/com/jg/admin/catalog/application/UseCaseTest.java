38c2378d201aba623507038462b5d3ca7e7574df
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-12-28 18:58:30 -0300

    create test valid command to create category

--- application/src/test/java/com/jg/admin/catalog/application/UseCaseTest.java
+++ application/src/test/java/com/jg/admin/catalog/application/UseCaseTest.java
@@ -1,15 +1,35 @@
 package com.jg.admin.catalog.application;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.jg.admin.catalog.domain.Identifier;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @ExtendWith(MockitoExtension.class)
-public class UseCaseTest {
+public abstract class UseCaseTest implements BeforeEachCallback {
 
-    @Test
-    public void testCreateUseCase() {
-
+    @Override
+    public void beforeEach(final ExtensionContext context) throws Exception {
+        Mockito.reset(getMocks().toArray());
     }
+
+    protected abstract List<Object> getMocks();
+
+//    protected Set<String> asString(final Set<? extends Identifier> ids) {
+//        return ids.stream()
+//                .map(Identifier::getValue)
+//                .collect(Collectors.toSet());
+//    }
+//
+//    protected List<String> asString(final List<? extends Identifier> ids) {
+//        return ids.stream()
+//                .map(Identifier::getValue)
+//                .toList();
+//    }
 }
---
+++ application/src/test/java/com/jg/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
@@ -0,0 +1,60 @@
+package com.jg.admin.catalog.application.category.create;
+
+import com.jg.admin.catalog.application.UseCaseTest;
+import com.jg.admin.catalog.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+public class CreateCategoryUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return null;
+    }
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, times(1)).create(argThat(aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.isNull(aCategory.getDeletedAt())
+        ));
+    }
+}
