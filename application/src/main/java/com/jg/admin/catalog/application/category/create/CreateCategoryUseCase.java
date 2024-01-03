28c5f58efd47ed3d8a89093426d249dd444fcd74
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-02 21:20:43 -0300

    use vavr.io

--- application/src/main/java/com/jg/admin/catalog/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/com/jg/admin/catalog/application/category/create/CreateCategoryUseCase.java
@@ -1,6 +1,8 @@
 package com.jg.admin.catalog.application.category.create;
 
 import com.jg.admin.catalog.application.UseCase;
+import com.jg.admin.catalog.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
