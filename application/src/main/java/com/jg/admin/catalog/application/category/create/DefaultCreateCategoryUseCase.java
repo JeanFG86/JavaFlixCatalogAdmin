7d9c7f204e8fdff016159fd1469634f07b05c04e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-02 21:14:03 -0300

    use notification pattern

--- application/src/main/java/com/jg/admin/catalog/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/jg/admin/catalog/application/category/create/DefaultCreateCategoryUseCase.java
@@ -2,6 +2,7 @@ package com.jg.admin.catalog.application.category.create;
 
 import com.jg.admin.catalog.domain.category.Category;
 import com.jg.admin.catalog.domain.category.CategoryGateway;
+import com.jg.admin.catalog.domain.validation.handler.Notification;
 import com.jg.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
 
 import java.util.Objects;
@@ -20,8 +21,13 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase{
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
 
+        final var notification = Notification.create();
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
-        aCategory.validate(new ThrowsValidationHandler());
+        aCategory.validate(notification);
+
+        if(notification.hasError()){
+
+        }
 
         return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
     }
