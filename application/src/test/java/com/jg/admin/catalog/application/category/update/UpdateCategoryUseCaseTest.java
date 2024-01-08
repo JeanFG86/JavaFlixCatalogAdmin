79b448762f0f21c0479a8ceb42254f8791b379fd
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-08 16:01:49 -0300

    ensure return Exception when gateway throws

--- application/src/test/java/com/jg/admin/catalog/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/jg/admin/catalog/application/category/update/UpdateCategoryUseCaseTest.java
@@ -115,4 +115,38 @@ public class UpdateCategoryUseCaseTest {
                         && aCategory.getUpdatedAt().isBefore(aUpdateCategory.getUpdatedAt())
                         && Objects.nonNull(aUpdateCategory.getDeletedAt())));
     }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
+        final var aCategory = Category.newCategory("Film", null, true);
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway error";
+        final var expectedErrorCount = 1;
+        final var expectedId = aCategory.getId();
+
+        final var aCommand =
+                UpdateCategoryCommand.with(expectedId.getValue(), expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.findById(eq(expectedId))).thenReturn(Optional.of(aCategory.clone()));
+
+        when(categoryGateway.update(any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var notification = useCase.execute(aCommand).getLeft();
+
+        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+
+        Mockito.verify(categoryGateway, times(1)).update(argThat(aUpdateCategory ->
+                Objects.equals(expectedName, aUpdateCategory.getName())
+                        && Objects.equals(expectedDescription, aUpdateCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aUpdateCategory.isActive())
+                        && Objects.equals(expectedId, aUpdateCategory.getId())
+                        && Objects.equals(aCategory.getCreatedAt(), aUpdateCategory.getCreatedAt())
+                        && aCategory.getUpdatedAt().isBefore(aUpdateCategory.getUpdatedAt())
+                        && Objects.isNull(aUpdateCategory.getDeletedAt())));
+    }
 }
