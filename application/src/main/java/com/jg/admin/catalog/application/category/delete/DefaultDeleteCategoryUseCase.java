3e31db9035e3f374ee2d5276691a2a1a2ee2a796
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-10 22:21:17 -0300

    create delete useCase

---
+++ application/src/main/java/com/jg/admin/catalog/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,20 @@
+package com.jg.admin.catalog.application.category.delete;
+
+import com.jg.admin.catalog.domain.category.CategoryGateway;
+import com.jg.admin.catalog.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase{
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public void execute(final String anIn) {
+        this.categoryGateway.deleteById(CategoryID.from(anIn));
+    }
+}
---
+++ application/src/main/java/com/jg/admin/catalog/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.jg.admin.catalog.application.category.delete;
+
+import com.jg.admin.catalog.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
