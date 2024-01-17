23c15bcd8039a07d47708c14dec08cd448b752c0
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-16 21:28:16 -0300

    configure springboot

--- infrastructure/src/main/java/com/jg/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/jg/admin/catalog/infrastructure/Main.java
@@ -1,11 +1,15 @@
 package com.jg.admin.catalog.infrastructure;
 
 import com.jg.admin.catalog.application.UseCase;
+import com.jg.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(new UseCase().execute());
+        SpringApplication.run(WebServerConfig.class, args);
 
     }
 }
