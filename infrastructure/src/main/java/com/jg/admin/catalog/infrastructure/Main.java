0904b3cb85227383bf60251ec8ef2fa152012b82
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-29 14:17:57 -0300

    alter server port

--- infrastructure/src/main/java/com/jg/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/jg/admin/catalog/infrastructure/Main.java
@@ -1,6 +1,5 @@
 package com.jg.admin.catalog.infrastructure;
 
-import com.jg.admin.catalog.application.UseCase;
 import com.jg.admin.catalog.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
