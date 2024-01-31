80ca338fa6583ef00a70b673e93dd802104184d2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-01-31 17:24:03 -0300

    create anotaion CategoryMySQLGatewayTest.java

--- infrastructure/src/test/java/com/jg/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/jg/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,27 +1,12 @@
 package com.jg.admin.catalog.infrastructure.category;
 
+import com.jg.admin.catalog.infrastructure.MySQLGatewayTest;
 import com.jg.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Collection;
-
-@ActiveProfiles("test")
-@DataJpaTest
-@ComponentScan(includeFilters = {
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*[MySQLGateway]")
-})
-@ExtendWith(CategoryMySQLGatewayTest.CleanUpExtensions.class)
+@MySQLGatewayTest
 public class CategoryMySQLGatewayTest {
 
     @Autowired
@@ -34,20 +19,5 @@ public class CategoryMySQLGatewayTest {
     public void testInjectedDependencies(){
         Assertions.assertNotNull(categoryGateway);
         Assertions.assertNotNull(categoryRepository);
-    }
-
-
-    static class CleanUpExtensions implements BeforeEachCallback{
-
-        @Override
-        public void beforeEach(ExtensionContext context) {
-            final var repositories =  SpringExtension.getApplicationContext(context).getBeansOfType(CrudRepository.class).values();
-
-            cleanUp(repositories);
-        }
-
-        private void cleanUp(final Collection<CrudRepository> repositories){
-            repositories.forEach(CrudRepository::deleteAll);
-        }
     }
 }
