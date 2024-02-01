8b56932c6da65f7bdd5cb208ccb2e8fb24374893
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-02-01 17:45:43 -0300

    should return error when null createdAt

--- infrastructure/src/test/java/com/jg/admin/catalog/infrastructure/category/persistence/CategoryRepositoryTest.java
+++ infrastructure/src/test/java/com/jg/admin/catalog/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -30,4 +30,21 @@ public class CategoryRepositoryTest {
         Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
         Assertions.assertEquals(expectedMessage, actualCause.getMessage());
     }
+
+    @Test
+    public void givenAnInvalidNullCreatedAt_WhenCallsSave_shouldReturnError(){
+        final var expectedPropertyName = "createdAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.jg.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity.createdAt";
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+
+        final var anEntity = CategoryJpaEntity.from(aCategory);
+        anEntity.setCreatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
 }
